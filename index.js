7663b4cb03bd85cf7b3045edd335bd4d55629e6d
HRSAC <canipelanthony@hotmail.fr>
2023-05-29 11:48:54 +0200

    ajout html

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // on appelle express (require module d'exprotation dans node.js)
 const express = require("express")
+const fs = require("fs")
 
 const app = express()
 
 const PORT = 8080
 
 app.get("/", (_, res) => {
-    res.send("bonjour")
+    const indexHTML = fs.readFileSync(__dirname + "/html/index.html.twig", "utf-8")
+    res.send(indexHTML)
 })
 
 //callback : fonction qui s'execute lorsque la ligne s'execute
