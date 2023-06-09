0f639c526fd0dab791d110f568c94402ad397335
HRSAC <canipelanthony@hotmail.fr>
2023-06-09 14:59:06 +0200

    ajour/react

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const app = express()
 
 const PORT = 8080
 
-app.get("/", (_, res) => {
-    const indexHTML = fs.readFileSync(__dirname + "/html/index.html.twig", "utf-8")
-    res.send(indexHTML)
-})
-
 //callback : fonction qui s'execute lorsque la ligne s'execute
 app.listen(PORT, () => {
     console.log("le serveur et lancé sur le port "+ PORT)
